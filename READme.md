023008ff3433cc1e12f1feb3f308dac75c24751b
DavidMandado <d.mandado@student.tue.nl>
2025-09-12 20:01:43 +0200

    more code

--- READme.md
+++ READme.md
@@ -3,3 +3,5 @@
 # Notes i dont want to forget
 
 - Optionalyl we can use ROI, we can use provided segmentation data or point labels grown into masks to focus bleaching classification strictly on coral pixels, this could cut the number of white sand = bleached flash positives and enables % bleached cover
+
+- i am putting as many comments in code as possible so everyone, including myself, can understand more whats going on, since i prefer to build the code slowly and understanding whats going on then mass producing malfunctioning scripts.
--- src/catalog/schema.py
+++ src/catalog/schema.py
@@ -20,4 +20,13 @@ class CatalogRow:
     height: Optional[int]
     qc_flags: Optional[str]
     
-    
+    def to_dict(self) -> Dict:
+        d = asdict(self) # this converts dataclass into python dict
+        d["qc_flags"] = ",".join(self.qc_flags) # turns a "mask_missing","size_mismatch" strin into "mask_missing,size_mismatch"
+        return d
+
+REQUIRED_COLUMNS = [
+    "dataset_name","dataset_group","image_path","mask_path",
+    "label_type","palette_key","has_points","points_path",
+    "site","transect","date","width","height","qc_flags"
+]
